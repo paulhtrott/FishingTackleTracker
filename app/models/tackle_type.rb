64c88cf3e02e285270619b93cb27f7d135df3d4e
Paul H Trott <paultrott78@gmail.com>
2014-03-29 14:31:33 -0500

    added name and category to model

--- app/models/tackle_type.rb
+++ app/models/tackle_type.rb
@@ -1,2 +1,6 @@
 class TackleType < ActiveRecord::Base
+  validates :name, :presence => true  
+
+  TACKLE_CATEGORIES = [ "Accessories", "Boating", "Electronics", "Fly Fishing", "Lures", "Reels", "Rods", "Terminal Tackle" ]
+  
 end
