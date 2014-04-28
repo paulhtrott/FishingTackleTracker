8f7fc9e0d36b2370d79bf72c9b4afb2db9504f47
Paul H Trott <paultrott78@gmail.com>
2014-04-28 13:18:59 -0500

    Added max size of 10MB for photos

--- app/models/member_tackle.rb
+++ app/models/member_tackle.rb
@@ -4,6 +4,7 @@ class MemberTackle < ActiveRecord::Base
   
   has_attached_file :tackle_img, styles: { thumb: '100x100>', medium: '300x300>' }
   validates_attachment_content_type :tackle_img, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_size :tackle_img, :in => 0..10.megabytes
 
   def self.latest
     MemberTackle.group("tackle_types(tackle_category)")
