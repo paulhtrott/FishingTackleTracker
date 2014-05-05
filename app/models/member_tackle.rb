f3f6c3832bbd6fa279839aca2abaec3207806667
Paul H Trott <paultrott78@gmail.com>
2014-05-04 23:09:52 -0500

    Fixed javascript file and member tackle mdoel for form, added custom field for tackle_category

--- app/models/member_tackle.rb
+++ app/models/member_tackle.rb
@@ -1,11 +1,13 @@
 class MemberTackle < ActiveRecord::Base
   belongs_to :user
   belongs_to :tackle_type
+
+  attr_accessor :custom_field
   
   has_attached_file :tackle_img, styles: { thumb: '100x100>', medium: '300x300>' }
   validates_attachment_content_type :tackle_img, :content_type => /\Aimage\/.*\Z/
   validates_attachment_size :tackle_img, :in => 0..10.megabytes
-
+  
   def self.latest
     MemberTackle.group("tackle_types(tackle_category)")
   end
