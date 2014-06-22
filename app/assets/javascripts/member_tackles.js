c5cc67cd3d6934c9ccfa2e078cb59a22f8b51ccb
Paul H Trott <paultrott78@gmail.com>
2014-06-21 21:20:32 -0500

    changed javascript to coffeescript

--- app/assets/javascripts/member_tackles.js
+++
@@ -1,9 +0,0 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-// You can use CoffeeScript in this file: http://coffeescript.org/
-
-$(function(){
-  $('#show-loading-btn').click(function(){
-    $('#show-loading-btn').attr('value', 'Adding Tackle...').addClass('disabled');
-  });
-})
