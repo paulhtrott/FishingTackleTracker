b4485f6cfffa33f3a18d060fd8bb88f78ce3670b
Paul H Trott <paultrott78@gmail.com>
2014-09-23 09:06:39 -0400

    Fixed dynamic loading of tackle categories and type options

--- app/controllers/javascripts_controller.rb
+++ app/controllers/javascripts_controller.rb
@@ -1,5 +1,6 @@
 class JavascriptsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
   def dynamic_tackle_type
-    @tackle = TackleType.find(:all)
+    @tackle = TackleType.all
   end
 end
